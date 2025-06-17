d85dd03e1c1be415ca93d50b9dd33fec701c12a3
Libina-Rai <libinarai@gmail.com>
2025-06-17 16:45:55 +0545

    chore:"Refactor content component"

--- part1_a/courseinfo/src/App.jsx
+++ part1_a/courseinfo/src/App.jsx
@@ -7,20 +7,26 @@ const Header = (props) =>{
   )
 }
 
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
 
-const Content = (props) => {
- 
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return(
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    <Part part={part1} exercises={exercises1}/>
+    <Part part={part2} exercises={exercises2}/>
+    <Part part={part3} exercises={exercises3}/>
     </div>
   )
 }
@@ -29,21 +35,16 @@ const Total = (props) => {
   return  <p>Number of exercises {props.sumOfExercises}</p>
 }
 const App = () => {
-   const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
+  const course = 'Half Stack application development'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
   const sumOfExercises = exercises1 + exercises2 + exercises3
   return (
     <div>
       
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part1={part2} exercises2={exercises2} />
-      <Content part1={part3} exercises3={exercises3} />
+      <Content />
       <Total sumOfExercises={sumOfExercises} />
     </div>
   )
