1540b5b4c6ce9265dfbf720b8e52f30d00cf85e5
Libina-Rai <libinarai@gmail.com>
2025-06-23 17:35:00 +0545

    chore:1.8(Refactor stat into separate component)

--- part1_a/unicafe/src/App.jsx
+++ part1_a/unicafe/src/App.jsx
@@ -1,24 +1,44 @@
 import { useState } from "react";
+const Header = () => {
+  return <h2>Give feedback</h2>;
+};
+
+const Content = () => {
+  return <h2>Statistics</h2>;
+};
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>All: {props.good + props.neutral + props.bad}</p>
+      <p>
+        average:{" "}
+        {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
+      </p>
+      <p>
+        positive:{" "}
+        {(props.good * 100) / (props.good + props.neutral + props.bad)} %
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <h2>Give feedback</h2>
+      <Header />
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h2>Statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>All: {good + neutral + bad}</p>
-      <p>average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive: {(good * 100) / (good + neutral + bad)} %</p>
+      <Content />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
