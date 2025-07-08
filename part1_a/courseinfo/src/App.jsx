07c105ef1db99e9cb9d3e27017470b0d1abc1d16
Libina-Rai <libinarai@gmail.com>
2025-07-08 14:47:45 +0545

    chore: map & render courses from array

--- part1_a/courseinfo/src/App.jsx
+++ part1_a/courseinfo/src/App.jsx
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-const Total = ({ parts }) => {
-  return (
-    <h4>
-      Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-    </h4>
-  );
-};
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "../Components/Course";
 const App = () => {
   const courses = [
     {
@@ -84,5 +54,5 @@ const App = () => {
   );
 };
 
-//  <Course course={course} />;
+
 export default App;
