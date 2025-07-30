c53917e4dd2fcd5b354d15d153409a04cda56a3f
Libina-Rai <libinarai@gmail.com>
2025-07-30 12:20:54 +0545

    feat: implement contact removal with DELETE method

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,35 +4,38 @@ const app = express();
 
 let persons = [
     { 
-      "id": "1",
+      "id": 1,
       "name": "Arto Hellas", 
       "number": "040-123456"
     },
     { 
-      "id": "2",
+      "id": 2,
       "name": "Ada Lovelace", 
       "number": "39-44-5323523"
     },
     { 
-      "id": "3",
+      "id": 3,
       "name": "Dan Abramov", 
       "number": "12-43-234345"
     },
     { 
-      "id": "4",
+      "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
 ];
 
+// Getting all persons
 app.get("/api/persons", (req, res) =>{
   res.send(persons);
 });
 
+// Getting persons information
 app.get("/info", (req, res) => {
   res.send(`Phonebook has info for ${persons.length} people.<br><br> ${Date()}`);
 })
 
+// Getting a person by id
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id) 
   const person = persons.find(person=>person.id === id);
@@ -42,6 +45,12 @@ app.get("/api/persons/:id", (req, res) => {
   res.send(person);
 });
 
+//Deleting a single person 
+app.delete("/api/persons/:id",(req, res)=>{
+  const id = Number(req.params.id);
+  let deletedPerson = persons.filter((person) => person.id !== id);
+  res.send(deletedPerson);
+})
 
 app.listen(3001, () =>{
   console.log("Server running on port 3001");
