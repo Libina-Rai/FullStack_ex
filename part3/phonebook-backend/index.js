6803f24c0b7fa87dea6e12798606c280f45e4d6a
Libina-Rai <libinarai@gmail.com>
2025-08-11 10:54:06 +0545

    feat: integrate CORS middleware and update base URL for API

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 //Defining a custom token for morgan to log the request body for POST request
 morgan.token("req-body", (req) => {
