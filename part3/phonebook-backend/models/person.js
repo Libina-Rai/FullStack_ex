25f037c1ba9863e92ebf71b9a18e0ce34c01ba39
Libina-Rai <libinarai@gmail.com>
2025-09-03 11:48:07 +0545

    feat: refactor App.jsx for readability and enhance phone number validation in person.js

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
@@ -8,8 +9,31 @@ mongoose
   .catch((err) => console.log("Error connecting to MongoDB:", err.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name must be at least 3 characters long"],
+  },
+  number: {
+    type: String,
+    required: [true, "Number is required"],
+    validate: {
+      validator: function (v) {
+        if (v.length < 8) return false;
+
+        const parts = v.split("-");
+        if (parts.length !== 2) return false;
+
+        const [first, second] = parts;
+        if (!/^\d{2,3}$/.test(first)) return false;
+        if (!/^\d+$/.test(second)) return false;
+
+        return true;
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Use format xx-xxxxxxx or xxx-xxxxxxx.`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
