0dd059e6f613b4c048ac62b44f10d8ab1330767c
Libina-Rai <libinarai@gmail.com>
2025-09-04 12:06:12 +0545

    chore: add ESLint setup with stylistic plugins and custom config

---
+++ part3/phonebook-backend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...globals.node },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs,
+    },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/quotes": ["error", "double"],
+      "@stylistic/js/semi": ["error", "always"],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
+      "no-empty": "off",
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+];
