2a5557c1439be35f3679786bb387c985372a019b
Libina-Rai <libinarai@gmail.com>
2026-03-09 14:14:05 +0545

    feat: implement favoriteBlog helper with unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return null
+  }
+
+  return blogs.reduce((favorite, current) => {
+    return (current.likes > favorite.likes) ? current : favorite
+  })
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
