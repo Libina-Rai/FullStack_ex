1cc555bee2ca9a7135040c30ab368c29bfe60a13
Libina-Rai <libinarai@gmail.com>
2026-03-09 13:37:01 +0545

    Refactor (bloglist): move blog schema to models, routes to controllers

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,37 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require("dotenv").config();
-// const config = require("./config") // if using .env
 
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+//mongodb connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.log(err))
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-const app = express()
-
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+// Routes
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003
 app.listen(PORT, () => {
