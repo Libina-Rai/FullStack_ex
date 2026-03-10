7a9e6c53d6a0c816b04bba60b950eb3635790b14
Libina-Rai <libinarai@gmail.com>
2026-03-10 17:17:24 +0545

    feat: add user model and router with password hashing functionality

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 app.use(express.json())
 
 // Routes
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
