f9c46e00b5e4ed646b739068ee00a526d426482a
Libina-Rai <libinarai@gmail.com>
2026-03-12 15:53:45 +0545

    feat: add login functionality and CORS support to the bloglist app

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/logins'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,56 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+  event.preventDefault()
+
+  try {
+    const user = await loginService.login({
+      username,
+      password
+    })
+
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  } catch (exception) {
+    console.log('wrong credentials', exception)
+  }
+}
+
+  if(user === null) {
+    return (
+      <div>
+        <h2>Login to application</h2>
+        <br/>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type='text'
+              value={username}
+              onChange={({ target }) => setUsername(target.value)} 
+            />
+            </div>
+            <div>
+            password
+            <input
+              type='password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            </div>
+            <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  // BLOG LIST (shown when user IS logged in)
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} is logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
