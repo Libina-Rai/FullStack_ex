9872d4aeac6cc4612536f54e0d3df51150b64b5e
Libina-Rai <libinarai@gmail.com>
2026-03-12 20:06:46 +0545

    feat: add Notification component with color-coded success/error messages for user actions

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,61 +3,85 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/logins'
 import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
+import './index.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
     if (loggedUserJSON) {
-    const user = JSON.parse(loggedUserJSON)
-    setUser(user)
-    blogService.setToken(user.token) // ensure axios calls use token
-  }
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
 
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   const handleLogin = async (event) => {
-  event.preventDefault()
+    event.preventDefault()
 
-  try {
-    const user = await loginService.login({
-      username,
-      password
-    })
-
+    try {
+    const user = await loginService.login({ username, password })
     setUser(user)
     window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
-    blogService.setToken(user.token) // ensure axios calls use token
+    blogService.setToken(user.token)
     setUsername('')
     setPassword('')
-  } catch (exception) {
-    console.log('wrong credentials', exception)
+  } catch (error) {
+    console.log('Login failed:', error.response?.data || error.message)
+    
+    // Make sure notification is an object with message + type
+    setNotification({
+      message: 'Wrong username or password',
+      type: 'error'
+    })
+
+    setTimeout(() => setNotification(null), 5000)
   }
-}
+  }
 
   const handleLogout = () => {
     setUser(null)
     window.localStorage.removeItem('loggedBlogAppUser')
-    blogService.setToken(null) // remove token from axios calls
+    blogService.setToken(null)
   }
 
   const addBlog = async (blogObject) => {
-  const returnedBlog = await blogService.create(blogObject)
-  setBlogs(blogs.concat(returnedBlog))
-}
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+
+      setBlogs(blogs.concat(returnedBlog))
+
+      setNotification({ message: `a new blog ${returnedBlog.title} added`, type: 'success' })
+
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+
+    } catch {
+      setNotification({ message: 'failed to add blog', type: 'error' })
+
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    }
+  }
 
   if(user === null) {
     return (
       <div>
         <h2>Login to application</h2>
         <br/>
+        <Notification message={notification?.message} type={notification?.type} />
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -66,25 +90,27 @@ const App = () => {
               value={username}
               onChange={({ target }) => setUsername(target.value)} 
             />
-            </div>
-            <div>
+          </div>
+          <div>
             password
             <input
               type='password'
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
-            </div>
-            <button type="submit">login</button>
+          </div>
+          <button type="submit">login</button>
         </form>
       </div>
     )
   }
 
-  // BLOG LIST (shown when user IS logged in)
   return (
     <div>
       <h2>blogs</h2>
+
+      <Notification message={notification?.message} type={notification?.type} />
+
       <p>{user.name} is logged in <button onClick={handleLogout}>logout</button></p>
 
       <BlogForm createBlog={addBlog} />
