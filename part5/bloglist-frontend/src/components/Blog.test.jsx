a905cbced6db962409e61695f7bed9538ada3bb9
Libina-Rai <libinarai@gmail.com>
2026-03-17 22:33:24 +0545

    test: add test for showing URL and likes after clicking view button

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest' 
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
 
@@ -27,4 +28,31 @@ test('renders title and author but not url or likes by default', () => {
   // hidden
   expect(screen.queryByText('http://example.com')).toBeNull()
   expect(screen.queryByText('likes 15')).toBeNull()
+})
+
+// show details after clicking "view"
+test('shows url and likes when view button is clicked', async () => {
+  const blog = {
+    title: 'Learning React',
+    author: 'Alex Johnson',
+    url: 'http://example.com',
+    likes: 15,
+    user: { name: 'Alex Johnson' }
+  }
+
+  render(
+    <Blog
+      blog={blog}
+      handleLike={() => {}}
+      handleRemove={() => {}}
+    />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton) // click to show details
+
+  // After clicking, URL and likes should be visible
+  expect(screen.getByText('http://example.com')).toBeInTheDocument()
+  expect(screen.getByText('likes 15')).toBeInTheDocument()
 })
