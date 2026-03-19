9f408a16b9f6c51a1bc947911ca0aa7a46c07cc3
Libina-Rai <libinarai@gmail.com>
2026-03-19 17:09:21 +0545

    feat: enhance login tests with user creation

--- part4/playwright/tests/login.spec.js
+++ part4/playwright/tests/login.spec.js
@@ -1,7 +1,18 @@
 const { describe, test, beforeEach, expect } = require('@playwright/test');
 
 describe('Login', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset') // reset the database before each test
+
+    //create a test user
+    const user = {
+      name: 'Priya Magar',
+      username: 'priya-mgr',
+      password: 'pri123456'
+    }
+    await request.post('http://localhost:3003/api/users', { data: user }) 
+
+    //visit the application
     await page.goto('http://localhost:5173'); 
   })
 
@@ -9,5 +20,21 @@ describe('Login', () => {
     await expect(page.getByText('Login to application')).toBeVisible()
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
+  })
+  
+  test('succesful login with correct credentials', async ({ page }) => {
+    await page.fill('input[name="username"]', 'priya-mgr')
+    await page.fill('input[name="password"]', 'pri123456')
+    await page.click('button[type="submit"]')
+
+    await expect(page.getByText('priya Magar is logged in')).toBeVisible()
+  })
+
+  test('failed with wrong credentials', async({ page }) => {
+    await page.fill('input[name="username"]', 'priya-mgr')
+    await page.fill('input[name="password"]', 'wrongpassword')
+    await page.click('button[type="submit"]')
+
+    await expect(page.getByText('wrong username or password')).toBeVisible()
   })
 })
