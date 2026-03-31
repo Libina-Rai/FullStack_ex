5a48177ac2b2a7d71dc77817982b56bf1b3104b4
Libina-Rai <libinarai@gmail.com>
2026-03-31 21:55:28 +0545

    feat: implement anecdote fetching and initial state setup

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,8 +1,21 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
-import Filter from './components/Filter'
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
+import Filter from "./components/Filter";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { create } from "./reducers/anecdoteReducer"; // use the existing create reducer
+import { getAll } from "./services/anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      const anecdotes = await getAll();
+      anecdotes.forEach((a) => dispatch(create(a.content)));
+    };
+    fetchAnecdotes();
+  }, [dispatch]);
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -10,7 +23,7 @@ const App = () => {
       <AnecdoteForm />
       <Filter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
