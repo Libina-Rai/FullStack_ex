1eaea1fe612b988b75b04c23766253ddda6c1918
Libina-Rai <libinarai@gmail.com>
2026-04-01 11:56:19 +0545

    feat: implement thunk for fetching anecdotes and update state management

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getAll, createNew } from "../services/anecdotes";
+import { getAll } from "../services/anecdotes";
 
 // Async thunk to fetch anecdotes from backend and initialize state
 export const initializeAnecdotes = createAsyncThunk(
@@ -7,15 +7,6 @@ export const initializeAnecdotes = createAsyncThunk(
   async () => {
     const anecdotes = await getAll();
     return anecdotes; // this payload will go to extraReducers
-  },
-);
-
-// Thunk to create a new anecdote
-export const createAnecdote = createAsyncThunk(
-  "anecdotes/createNew",
-  async (content) => {
-    const newAnecdote = await createNew(content);
-    return newAnecdote;
   },
 );
 
@@ -30,19 +21,18 @@ const anecdoteSlice = createSlice({
         anecdote.votes += 1;
       }
     },
+    create(state, action) {
+      state.push(action.payload);
+    },
   },
-
+  
   //handle the thunk results in extraReducers
   extraReducers: (builder) => {
-    builder
-      .addCase(createAnecdote.fulfilled, (state, action) => {
-        state.push(action.payload);
-      })
-      .addCase(initializeAnecdotes.fulfilled, (state, action) => {
-        return action.payload;
-      });
+    builder.addCase(initializeAnecdotes.fulfilled, (state, action) => {
+      return action.payload; // replace state with backend data
+    });
   },
 });
 
-export const { vote } = anecdoteSlice.actions;
+export const { vote, create } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
