fc3a5bc0fa3630dc66ab2fb4669ba67fbe5185a6
Libina-Rai <libinarai@gmail.com>
2026-04-03 13:10:17 +0545

    fix: downgrade @tanstack/react-query to version 5.95.2 and remove anecdote initialization logic from App component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,20 +1,9 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 import Notification from "./components/Notification";
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-     dispatch(initializeAnecdotes())
-      .catch((error) => {
-        console.error("Failed to fetch anecdotes:", error);
-      });
-  }, [dispatch]);
 
   return (
     <div>
