72c61e28facf61a4ae611de9cbcae7dbad3ca394
Libina-Rai <libinarai@gmail.com>
2026-04-09 18:00:52 +0545

    feat: implement useAnecdotes hook for fetching anecdotes from service

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import anecdoteService from "../services/anecdotes";
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([]);
+
+  useEffect(() => {
+    anecdoteService.getAll().then((data) => {
+      setAnecdotes(data);
+    });
+  }, []);
+
+  return anecdotes;
+};
 
 export const useField = (type = "text") => {
   const [value, setValue] = useState("");
